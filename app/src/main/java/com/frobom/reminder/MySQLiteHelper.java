d2fc15a4a39cc01a2c7ab30664df54966e5e2dd2
KyawMinHtwe <kyaw.min.htwe@frobom.com>
2017-06-30 13:52:22 +0630

    coding rules check and database updated

--- app/src/main/java/com/frobom/reminder/MySQLiteHelper.java
+++ app/src/main/java/com/frobom/reminder/MySQLiteHelper.java
@@ -13,13 +13,14 @@ import android.widget.Toast;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_NAME = "reminder";
-    public static final String COLUMN_ID = "id";
-    public static final String REMIND_TITLE = "title";
-    public static final String REMIND_CONTENT="description";
-    public  static final String TIME="time";
-    public static  final String DATE="date";
-    public static final String REMIND_URI="remindUri";
+    public static final String TABLE_NAME = "reminder_table";
+    public static final String id = "id";
+    public static final String title = "title";
+    public static final String description = "description";
+    public  static final String time = "time";
+    public static  final String date = "date";
+    public static final String alarm_path = "alarm_path";
+    public static final String is_delete = "is_delete";
     public static SQLiteDatabase db;
     private static final String DATABASE_NAME = "reminderDB.db";
     private static final int DATABASE_VERSION = 1;
@@ -27,10 +28,14 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     private Activity activity;
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
-            + TABLE_NAME + "( " + COLUMN_ID
-            + " integer primary key autoincrement, " + REMIND_TITLE
-            + " text not null, " + REMIND_CONTENT + " text not null, "+ TIME + " text not null, " +
-            DATE + " text not null, " + REMIND_URI + " text not null);";
+            + TABLE_NAME + "( "
+            + id + " integer primary key autoincrement, "
+            + title + " text not null, "
+            + description + " text not null, "
+            + time + " text not null, "
+            + date + " text not null, "
+            + alarm_path + " text not null, "
+            + is_delete + " bool not null );";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
