6a7334c7f37fb56ced112bab3934b2ae0c7cd6d4
KyawMinHtwe <kyaw.min.htwe@frobom.com>
2017-07-07 17:09:37 +0630

    fab on list and setting

--- app/src/main/java/com/frobom/reminder/MainActivity.java
+++ app/src/main/java/com/frobom/reminder/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -78,6 +81,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setIconInMenu(Menu menu, int menuItemId, int labelId, int iconId){
+        MenuItem menuItem = menu.findItem(menuItemId);
+        SpannableStringBuilder sBuilder = new SpannableStringBuilder("        " + getResources().getString(labelId));
+        sBuilder.setSpan(new ImageSpan(this, iconId), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        menuItem.setTitle(sBuilder);
+    }
+
     @Override
     protected void onResume() {
         datasource.open();
@@ -95,6 +105,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        setIconInMenu(
+                menu,
+                R.id.action_help,
+                R.string.action_help,
+                R.drawable.ic_action_help
+        );
+
+        setIconInMenu(
+                menu,
+                R.id.action_feedback,
+                R.string.action_feedback,
+                R.drawable.ic_action_email
+        );
+
         return true;
     }
 
@@ -106,7 +131,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_help) {
+            return true;
+        }
+
+        if (id == R.id.action_feedback) {
             return true;
         }
 
