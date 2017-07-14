bbae3536ee6c960dbb9fe522d49b72ea736c3c4c
Lin <soe.yel.linn@frobom.com>
2017-07-14 11:50:15 +0630

    Fixed and changed media player (Alarm Service and Alarm activity).

--- app/src/main/java/com/frobom/reminder/AlarmService.java
+++ app/src/main/java/com/frobom/reminder/AlarmService.java
@@ -2,27 +2,13 @@ package com.frobom.reminder;
 
 import android.app.Service;
 import android.content.Intent;
-import android.media.AudioTrack;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.PowerManager;
 import android.support.annotation.Nullable;
 
-import java.io.IOException;
-import java.util.List;
-
-public class AlarmService extends Service
-{
-
-    MediaPlayer mediaPlayer;
+public class AlarmService extends Service{
     int DD;
-    int idgot;
-    String path="";
-    public DatabaseAccessAdapter datasource;
+
 
     @Nullable
     @Override
@@ -50,64 +36,14 @@ public class AlarmService extends Service
         b.putInt("id", DD);
         intent.putExtras(b);
 
-        setData(DD);
-
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioTrack.MODE_STREAM);
-
-        //String filePath = Environment.getExternalStorageDirectory()+path;
-        //mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.redzonefullmix);
-
-
-        mediaPlayer = MediaPlayer.create(AlarmService.this, Uri.parse(Environment.getExternalStorageDirectory().getPath()+ path));
-        //mediaPlayer.setDataSource(filePath);
-
-
-        //mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-        if(mediaPlayer != null && mediaPlayer.isPlaying() != true)
-        {
-            mediaPlayer.setVolume(50, 50);
-            mediaPlayer.setScreenOnWhilePlaying(true);
-            mediaPlayer.setLooping(true);
-
-            mediaPlayer.start();
-        }
     }
-
-    public void setData(int idGet)
-    {
-        idgot=idGet;
-
-        datasource = new DatabaseAccessAdapter(this);
-        datasource.open();
-
-        List<Attributes> values = datasource.getAllAttributes();
-
-
-        if(values.size()>0) {
-            for (int i = 0; i < values.size(); i++)
-            {
-                if(idgot == values.get(i).getId())
-                {
-                    path = values.get(i).getAlarmPath();
-                    break;
-                }
-            }
-        }
-
-        datasource.close();
-    }
-
 
     @Override
     public void onDestroy()
     {
         super.onDestroy();
-        mediaPlayer.stop();
-        mediaPlayer.release();
         stopService(new Intent(this, ReminderAlarmManger.class));
         startService(new Intent(this, ReminderAlarmManger.class));
     }
--- app/src/main/java/com/frobom/reminder/alarm.java
+++ app/src/main/java/com/frobom/reminder/alarm.java
@@ -1,6 +1,14 @@
 package com.frobom.reminder;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
+import android.os.PowerManager;
+import android.support.v13.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,30 +19,88 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.List;
 
-public class alarm extends AppCompatActivity {
+public class alarm extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
 
     public DatabaseAccessAdapter datasource;
     private int idgot;
+    private int trigger = 0;
+
+    MediaPlayer mediaPlayer;
+
+    public String path;
 
     public TextView Title;
     public TextView Clock;
     public TextView Date;
     public TextView Content;
 
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
+        trigger=0;
 
         Bundle b = this.getIntent().getExtras();
         int id = b.getInt("id");
 
+        verifyStoragePermissions(alarm.this);
+
         setData(id);
 
+        mediaPlay();
+
         closeActivity();
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    private void mediaPlay()
+    {
+        mediaPlayer = new MediaPlayer();
+        AudioManager audioManager = (AudioManager) getSystemService(this.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
+
+        try
+        {
+            mediaPlayer.setDataSource(path);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setAudioStreamType(AudioTrack.MODE_STREAM);
+        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mediaPlayer.setVolume(50,50);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setOnPreparedListener(alarm.this);
+        mediaPlayer.prepareAsync();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp)
+    {
+        mp.start();
     }
 
     private void setData(int idGet)
@@ -63,6 +129,7 @@ public class alarm extends AppCompatActivity {
                     String time = values.get(i).getAlarmTime();
                     String date = values.get(i).getAlarmDate();
                     String description = values.get(i).getDescription();
+                    path = values.get(i).getAlarmPath();
 
                     Title.setText(title);
                     Clock.setText(time);
@@ -106,6 +173,9 @@ public class alarm extends AppCompatActivity {
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                trigger=1;
+                mediaPlayer.stop();
+                mediaPlayer.release();
                 finish();
             }
         });
@@ -115,17 +185,26 @@ public class alarm extends AppCompatActivity {
     protected void onPause()
     {
         super.onPause();
+
         Intent intent = new Intent(alarm.this, AlarmService.class);
         alarm.this.stopService(intent);
 
         stopService(new Intent(this, ReminderAlarmManger.class));
         startService(new Intent(this, ReminderAlarmManger.class));
 
+        finish();
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        if(mediaPlayer != null && trigger==0)
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
 
         Intent intent = new Intent(alarm.this, AlarmService.class);
         alarm.this.stopService(intent);
