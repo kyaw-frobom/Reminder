32768fab8a63a239bbfdbbea6c5c7515c3e7ca37
Lin <soe.yel.linn@frobom.com>
2017-08-03 08:54:51 +0630

    renew location based alarm page!

--- app/src/main/java/com/frobom/reminder/AddLocationActivity.java
+++ app/src/main/java/com/frobom/reminder/AddLocationActivity.java
@@ -2,18 +2,28 @@ package com.frobom.reminder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
 
 /**
  * Created by ソーイエーリン on 01-Aug-17.
  */
 
-public class AddLocationActivity extends AppCompatActivity
-{
+public class AddLocationActivity extends AppCompatActivity{
+
+    private Spinner spinner1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
-        //for back button
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addListenerOnSpinnerItemSelection();
+    }
+
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 }
---
+++ app/src/main/java/com/frobom/reminder/CustomOnItemSelectedListener.java
@@ -0,0 +1,22 @@
+package com.frobom.reminder;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by ソーイエーリン on 02-Aug-17.
+ */
+
+class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener
+{
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+}
